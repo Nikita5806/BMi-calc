711b8471f7510c539e159a539c0a05bdb7fafd6e
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-02-07 23:50:54 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -28,3 +28,50 @@ let callbmi =(e) =>{
   }
 }
 //reload
+  let  reload =()=> {
+  window.location.reload()
+}
+
+
+
+
+
+
+
+
+
+
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h2>BMI CALCULATOR</h2>
+        <form onSubmit ={callbmi}>
+          <div>
+            <label> Weight </label>
+            <input type="text" placeholder='enter weight' value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </div>
+          <div>
+            <label> Height </label>
+            <input type="text" placeholder='enter height' value={height}
+            onChange={(e) => setHeight(e.target.value)} />
+          </div>
+          <div>
+            <button className='btn' type='submit'>submit</button>
+            <button className='btn-btn-outline' onClick={reload} type='submit'>Reload</button>
+          </div>
+
+          <div className='center'>
+            <h3>Your BMI is: {bmi}</h3>
+            <p>{message}</p>
+
+          </div>
+        </form>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
