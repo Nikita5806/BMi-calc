d5c3fc0f13137f5db77a3756e2a684a827e3d565
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-02-07 23:49:44 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,4 +1,8 @@
 mport React from "react";
 import { useState } from "react";
 
-
+function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState('');
+  const [message, setMessage] = useState('');
