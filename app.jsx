ef949ae0b0f7edbe60b04721a7d1ffddcee5159a
Nikita kanwar <nikita.ns321ns@gmail.com>
2025-02-07 23:50:37 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -6,3 +6,25 @@ function App() {
   const [height, setHeight] = useState(0);
   const [bmi, setBmi] = useState('');
   const [message, setMessage] = useState('');
+//logic
+let callbmi =(e) =>{
+  event.preventDefault();
+  if (weight ===0 || height ===0 ){
+    alert ('please enter a valid signs')
+  }
+    else{
+      let bmi = (weight / (height*height)*703)
+      setBmi(bmi.toFixed(1))
+    //
+    if(bmi <25 ){
+      setMessage ('you are  bhoutt patleeee')
+    }else if(bmi>=25 && bmi <30){
+      setMessage('bhaii tu fittt hhhh')
+    }else {
+      setMessage('motiyeeee')
+    }
+
+    
+  }
+}
+//reload
